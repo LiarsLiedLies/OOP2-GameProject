69992fa6f155e4df5268dbb5a128564660bccca0
ArnnonZevv <arnnon.pangan123@gmail.com>
2025-03-26 22:25:33 +0800

    Add files via upload

---
+++ GameTime.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gametime;
+
+/**
+ *
+ * @author PaNgAn
+ */
+public class GameTime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
